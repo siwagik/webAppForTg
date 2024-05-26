f4297c57d30a004d9e94467b683cfa0a8965d37a
siwagik <siwagejob@gmail.com>
2024-05-26 17:14:48 +0300

    beta

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
       <h1>Hello {tg.initDataUnsafe.user?.username}</h1>
+      <h1>Your system: {tg.platform}</h1>
       <h1>Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Click me</button>
     </>
