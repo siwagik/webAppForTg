0ad77060a46f8c5a1a8b03a7ff3c64053e2ba40f
siwagik <siwagejob@gmail.com>
2024-05-26 15:24:05 +0300

    beta

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import './App.css'
 
+const tg = window.Telegram.WebApp;
+
 function App() {
 
   const [count, setCount ] = useState(0)
 
   return (
     <>
+      <h1>Hello {tg.initDataUnsafe.user?.username}</h1>
       <h1>Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Click me</button>
     </>
