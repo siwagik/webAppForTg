8b73da49137b95ddee506ffc3d7dbbf587d5a771
siwagik <siwagejob@gmail.com>
2024-08-16 19:49:53 +0300

    baza

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,62 @@
 import { useState } from 'react'
 import logo from '/click.png'
 const tg = window.Telegram.WebApp
+
 function App() {
 	const [count, setCount] = useState(0)
-	const [animate, setAnimate] = useState(false)
+	const [animations, setAnimations] = useState([])
 
 	const click = e => {
 		setCount(count + 1)
+		navigator.vibrate(50)
+
+		const buttonRect = e.target.getBoundingClientRect()
+		const x = e.clientX - buttonRect.left
+		const y = e.clientY - buttonRect.top
+		const id = Date.now() // уникальный идентификатор для каждой анимации
+
+		setAnimations(prevAnimations => [...prevAnimations, { id, x, y }])
+
+		setTimeout(() => {
+			setAnimations(prevAnimations =>
+				prevAnimations.filter(animation => animation.id !== id)
+			)
+		}, 1000) // Удаление анимации через 1 секунду
 	}
 
 	return (
 		<div className='flex flex-col justify-center items-center bg-[#0e1b19] h-screen text-white'>
 			<div>
-				<h1 className='text-red-500'>
-					Hello, ${tg.initDataUnsafe.user.username}
+				<h1 className='text-white font-medium mb-24 bg-[#254742]/50 px-2 py-2 rounded-xl'>
+					Hello,{' '}
+					<span className='text-[#5EC8B5]'>
+						{tg.initDataUnsafe.user.username}
+					</span>
 				</h1>
 			</div>
-			<div className='flex items-center'>
-				<img src={logo} className='w-12 h-12' />
-				<h1 className='text-3xl font-bold ml-2'>{count}</h1>
+			<div className='flex flex-col items-center'>
+				<h1 className='text-xl text-[#ffffff9f]'>Your balance</h1>
+				<h1 className='text-3xl font-semibold ml-2'>$ {count}</h1>
 			</div>
-			<div>
-				<button onClick={click}>
+			<div className='relative'>
+				<button onClick={click} className='relative'>
 					<img src={logo} />
+					{animations.map(animation => (
+						<div
+							key={animation.id}
+							className='absolute text-3xl font-bold text-white animate-fade-out-up pointer-events-none'
+							style={{
+								left: `${animation.x}px`,
+								top: `${animation.y}px`,
+							}}
+						>
+							+1
+						</div>
+					))}
 				</button>
 			</div>
-			<div className='bg-black/25 px-10 py-5 rounded-lg'>
-				<a href='#' className='text-xl'>
+			<div className='mt-24'>
+				<a href='#' className='bg-black/25 px-10 py-5 rounded-lg text-xl'>
 					Withdrawal
 				</a>
 			</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				'fade-out-up': {
+					'0%': { opacity: 1, transform: 'translateY(0)' },
+					'100%': { opacity: 0, transform: 'translateY(-20px)' },
+				},
+				moveStar: {
+					'0%': { transform: 'translate(-50%, -50%) translate(0%, 0%)' },
+					'100%': {
+						transform: 'translate(-50%, -50%) translate(100vw, 100vh)',
+					},
+				},
+			},
+			animation: {
+				'fade-out-up': 'fade-out-up 1s ease-in-out forwards',
+				'move-star': 'moveStar 3s linear infinite',
+			},
+		},
 	},
 	plugins: [],
 }
